72af18b29e1e5c69bd3b9b9944a056b9a7641210
Eric Thill <code@thill.io>
2019-02-23 16:09:20 -0600

    Delete topic fix and QueuedKafkaConsumer single thread

--- src/main/java/io/thill/kafkalite/client/QueuedKafkaConsumer.java
+++ src/main/java/io/thill/kafkalite/client/QueuedKafkaConsumer.java
@@ -8,12 +8,9 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Properties;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Queue;
 
 /**
  * Queueing Kafka Consumer, useful for tests since it can poll 1 message at a time with a timeout
@@ -23,9 +20,7 @@ public class QueuedKafkaConsumer<K, V> implements AutoCloseable {
   private static final Duration KAFKA_POLL_DURATION = Duration.ofMillis(100);
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
-  private final AtomicBoolean keepRunning = new AtomicBoolean(true);
-  private final CountDownLatch closedLatch = new CountDownLatch(1);
-  private final BlockingQueue<ConsumerRecord<K, V>> queue = new LinkedBlockingQueue<>();
+  private final Queue<ConsumerRecord<K, V>> queue = new LinkedList<>();
   private final TopicPartition topicPartition;
   private final KafkaConsumer<K, V> consumer;
 
@@ -33,8 +28,7 @@ public class QueuedKafkaConsumer<K, V> implements AutoCloseable {
     this.topicPartition = topicPartition;
     consumer = new KafkaConsumer<>(properties);
     consumer.assign(Arrays.asList(topicPartition));
-    consumer.seekToBeginning(Arrays.asList(topicPartition));
-    new Thread(this::run).start();
+    seekToBeginning();
   }
 
   /**
@@ -51,50 +45,28 @@ public class QueuedKafkaConsumer<K, V> implements AutoCloseable {
     consumer.seekToEnd(Arrays.asList(topicPartition));
   }
 
-  private void run() {
-    try {
-      while(keepRunning.get()) {
-        for(ConsumerRecord<K, V> record : consumer.poll(KAFKA_POLL_DURATION)) {
-          queue.add(record);
-        }
-      }
-    } finally {
-      closedLatch.countDown();
-    }
-  }
-
   /**
    * Poll a single record, waiting for up to one second for it to be available. After 1 second, null is returned.
    *
    * @return The record, or null if it did not exist within 1 second
    */
   public ConsumerRecord<K, V> poll() {
-    return poll(1, TimeUnit.SECONDS);
+    return poll(Duration.ofSeconds(1));
   }
 
   /**
-   * Poll a single record, waiting for up to the given number of milliseconds for it to be available. After the given number of milliseconds, null is returned.
+   * Poll a single record, waiting for up to the given timeout for it to be available. After the timeout, null is returned.
    *
-   * @param timeoutMillis The timeout in milliseconds
-   * @return The record, of null if it did not exist within the given number of milliseconds
-   */
-  public ConsumerRecord<K, V> poll(long timeoutMillis) {
-    return poll(timeoutMillis, TimeUnit.MILLISECONDS);
-  }
-
-  /**
-   * Poll a single record, waiting for up to the given time/timeUnit for it to be available. After the timeout, null is returned.
-   *
-   * @param timeout The timeout value
-   * @param unit    The unit of the timeout
+   * @param timeout The timeout duration
    * @return The record, of null if it did not exist within the timeout
    */
-  public ConsumerRecord<K, V> poll(long timeout, TimeUnit unit) {
-    try {
-      return queue.poll(timeout, unit);
-    } catch(InterruptedException e) {
-      return null;
+  public ConsumerRecord<K, V> poll(Duration timeout) {
+    if(queue.size() == 0) {
+      for(ConsumerRecord<K, V> r : consumer.poll(timeout)) {
+        queue.add(r);
+      }
     }
+    return queue.poll();
   }
 
   public boolean isEmpty() {
@@ -112,11 +84,6 @@ public class QueuedKafkaConsumer<K, V> implements AutoCloseable {
 
   @Override
   public void close() {
-    keepRunning.set(false);
-    try {
-      closedLatch.await();
-    } catch(InterruptedException e) {
-      logger.warn("Interrupted", e);
-    }
+    consumer.close();
   }
 }
